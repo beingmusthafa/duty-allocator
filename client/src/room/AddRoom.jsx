0b1a5b8d294ddf257a8d848dfddadde3b0c57d52
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-05-02 00:01:43 +0530

    ADD room form validation

--- client/src/room/AddRoom.jsx
+++ client/src/room/AddRoom.jsx
@@ -1,49 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Input, Button } from '@material-tailwind/react';
-import axios from 'axios';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Input, Button } from "@material-tailwind/react";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useForm } from "react-hook-form";
 
 function AddRoom() {
-  const [formData, setFormData] = useState({
-    roomname: "",
-    seats: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const change = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const submit = (e) => {
-    e.preventDefault();
-    axios.post("http://localhost:3000/room/add", formData)
+  const submit = (data) => {
+    const { roomname, seats } = data;
+    axios
+      .post("http://localhost:3000/room/add", { roomname, seats })
       .then((res) => {
-        toast.success('Room added successfully', {
+        toast.success("Room added successfully", {
           position: "top-center",
           autoClose: 1000,
           closeButton: false,
-          style: { borderRadius: "100px" }
+          style: { borderRadius: "100px" },
         });
         console.log("successful", res.data);
       })
       .catch((error) => {
         if (error.response.data.error === "Room already exists") {
-          toast.error('Room already exists', {
+          toast.error("Room already exists", {
             position: "top-center",
             autoClose: 1000,
             closeButton: false,
-            style: { borderRadius: "100px" }
+            style: { borderRadius: "100px" },
           });
         } else {
-          toast.error('Error adding room', {
+          toast.error("Error adding room", {
             position: "top-center",
             autoClose: 1000,
             closeButton: false,
-            style: { borderRadius: "100px" }
+            style: { borderRadius: "100px" },
           });
         }
         console.log("error", error.response.data);
@@ -52,23 +48,29 @@ function AddRoom() {
 
   return (
     <div className="max-w-md mx-auto pl-8 pr-8 pt-4 mt-20 pb-5 bg-white shadow-md rounded-md">
-      <h2 className="text-2xl font-bold text-center mb-4 uppercase">Add Room</h2>
-      <form onSubmit={submit} className="space-y-5">
+      <h2 className="text-2xl font-bold text-center mb-4 uppercase">
+        Add Room
+      </h2>
+      <form onSubmit={handleSubmit(submit)} className="space-y-5">
+        <p className="text-red-500 text-sm">{errors.roomname?.message}</p>
         <Input
+          {...register("roomname", {
+            required: "Enter room name",
+            maxLength: { value: 10, message: "Name too long" },
+          })}
           type="text"
           label="Room name"
           name="roomname"
-          value={formData.roomname}
-          onChange={change}
-          required
         />
+        <p className="text-red-500 text-sm">{errors.seats?.message}</p>
         <Input
-          type="text"
+          {...register("seats", {
+            required: "Enter no. of seats",
+            max: { value: 30, message: "Exceeds limit of 30" },
+            validate: (value) => value >= 4 || "Should be minimum 4",
+          })}
+          type="number"
           label="No. of Seats"
-          name="seats"
-          value={formData.seats}
-          onChange={change}
-          required
         />
         <Button
           type="submit"
