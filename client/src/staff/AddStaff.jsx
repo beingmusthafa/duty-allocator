0ca171333e2a26283c810078f942dda610841931
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-05-01 21:15:30 +0530

    ADD staff form validation

--- client/src/staff/AddStaff.jsx
+++ client/src/staff/AddStaff.jsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Input, Button } from '@material-tailwind/react';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Input, Button, Select, Option } from "@material-tailwind/react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
 function AddStaff() {
   const [deptList, setDeptList] = useState([]);
-  const [fName, setFName] = useState('');
-  const [lName, setLName] = useState('');
-  const [phNumber, setPhNumber] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [designation, setDesignation] = useState('invigilator');
-  const [dept, setDept] = useState('');
+  const [dept, setDept] = useState("");
   const [showOptions, setShowOptions] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const password = watch("password");
 
   useEffect(() => {
     fetchDeptDetails();
@@ -22,166 +23,174 @@ function AddStaff() {
 
   const fetchDeptDetails = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/dept/view');
+      const response = await axios.get("http://localhost:3000/dept/view");
       setDeptList(response.data);
     } catch (error) {
-      console.error('Error fetching department details:', error);
+      console.error("Error fetching department details:", error);
     }
   };
-  const role = (designation === 'invigilator') ? 'staff' : 'hod';
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      return toast.error('Password Doesnt Match', {
-        position: "top-center",
-        autoClose: 1000,
-        closeButton: false,
-        style: { borderRadius: "100px" }
-      });
-    }
+  const onSubmit = async (data) => {
+    const { fName, lName, phNumber, email, password, designation, dept } = data;
+    const role = designation === "invigilator" ? "staff" : "hod";
     try {
-      await axios.post('http://localhost:3000/staff/add', {
-        fName, lName, phNumber, email, password, designation, dept, role
+      await axios.post("http://localhost:3000/staff/add", {
+        fName,
+        lName,
+        phNumber,
+        email,
+        password,
+        designation,
+        dept,
+        role,
       });
 
-      toast.success('Staff added successfully');
+      toast.success("Staff added successfully");
       clearForm();
     } catch (error) {
       if (error.response.data.error === "Email already exists") {
-        toast.error('Email already exists');
+        toast.error("Email already exists");
       } else if (error.response.data.error === "Phone number already exists") {
-        toast.error('Phone number already exists');
+        toast.error("Phone number already exists");
       } else {
-        toast.error('Error adding staff');
+        toast.error("Error adding staff");
       }
     }
   };
 
   const clearForm = () => {
-    setFName('');
-    setLName('');
-    setPhNumber('');
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
-    setDesignation('invigilator');
-    setDept('');
+    //clear all form inputs
   };
 
   return (
     <div className="">
       <div className="max-w-lg mx-auto pl-8 pr-8 pt-4 mt-11 pb-2 bg-white shadow-md rounded-md mt-4">
-        <h2 className="text-2xl font-bold text-center mb-4 uppercase">Add Staff</h2>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <Input
-            type="text"
-            label="First Name"
-            value={fName}
-            onChange={(e) => setFName(e.target.value)}
-            required
-          />
-          <Input
-            type="text"
-            label="Last Name"
-            value={lName}
-            onChange={(e) => setLName(e.target.value)}
-            required
-          />
+        <h2 className="text-2xl font-bold text-center mb-4 uppercase">
+          Add Staff
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <div className="flex flex-col">
+            <p className="text-red-500 text-sm mb-1">{errors.fName?.message}</p>
+            <Input
+              {...register("fName", {
+                required: "Enter first name",
+                minLength: { value: 3, message: "Name too short" },
+                maxLength: { value: 10, message: "Name too long" },
+              })}
+              type="text"
+              label="First Name"
+            />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-red-500 text-sm mb-1">{errors.lName?.message}</p>
+            <Input
+              {...register("lName", {
+                required: "Enter last name",
+                minLength: { value: 3, message: "Name too short" },
+                maxLength: { value: 10, message: "Name too long" },
+              })}
+              type="text"
+              label="Last Name"
+            />
+          </div>
           <div className="flex space-x-4 ">
-            <div className="w-72 relative ">
-              <div className="relative inline-block w-full">
-                <div
-                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-400 cursor-pointer"
-                  onClick={() => setShowOptions(!showOptions)}
-                >
-                  <div className="flex justify-between items-center">
-                    <span className="text-gray-600">
-                      {dept || 'Department'}
-                    </span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      className="w-4 h-4 text-gray-600"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </div>
-                </div>
-                {showOptions && (
-                  <div className="absolute w-full bg-white border border-gray-300 rounded-md mt-1 z-10">
-                    {deptList.map((deptItem) => (
-                      <div
-                        key={deptItem._id}
-                        className="cursor-pointer px-3 py-2 hover:bg-gray-100"
-                        required
-                        onClick={() => {
-                          setDept(deptItem.dept);
-                          setShowOptions(false);
-                        }}
-                      >
-                        {deptItem.dept}
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
+            <div className="flex flex-col">
+              <p className="text-red-500 text-sm mb-1">
+                {errors.dept?.message}
+              </p>
+              <select
+                className="p-2 outline-none border"
+                {...register("dept", { required: "Select a department" })}
+              >
+                <option value="">Department</option>
+                {deptList.map((item) => (
+                  <option className="p-2" key={item.dept} value={item.dept}>
+                    {item.dept}
+                  </option>
+                ))}
+              </select>
             </div>
+            <div className="flex flex-col">
+              <p className="text-red-500 text-sm mb-1">
+                {errors.phNumber?.message}
+              </p>
+              <Input
+                {...register("phNumber", {
+                  required: "Enter phone number",
+                  minLength: { value: 10, message: "Invalid phone number" },
+                  maxLength: { value: 10, message: "Invalid phone number" },
+                })}
+                type="number"
+                label="Phone Number"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-red-500 text-sm mb-1">{errors.email?.message}</p>
             <Input
-              type="number"
-              label="Phone Number"
-              value={phNumber}
-              onChange={(e) => setPhNumber(e.target.value)}
-              required
+              {...register("email", {
+                required: "Enter email",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Invalid email",
+                },
+              })}
+              type="email"
+              label="Email"
             />
           </div>
-          <Input
-            type="email"
-            label="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
           <div className="flex space-x-4">
-            <Input
-              type="password"
-              label="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <Input
-              type="password"
-              label="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
+            <div className="flex flex-col">
+              <p className="text-red-500 text-sm mb-1">
+                {errors.password?.message}
+              </p>
+              <Input
+                {...register("password", {
+                  required: "Enter password",
+                  minLength: { value: 8, message: "Password too short" },
+                  maxLength: { value: 16, message: "Password too long" },
+                })}
+                type="password"
+                label="Password"
+              />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-red-500 text-sm mb-1">
+                {errors.confirmPassword?.message}
+              </p>
+              <Input
+                {...register("confirmPassword", {
+                  required: "Enter password",
+                  minLength: { value: 8, message: "Password too short" },
+                  maxLength: { value: 16, message: "Password too long" },
+                  validate: (value) =>
+                    value === password || "Passwords do not match",
+                })}
+                type="password"
+                label="Confirm Password"
+              />
+            </div>
           </div>
+          <p className="text-red-500 text-sm mb-1">
+            {errors.designation?.message}
+          </p>
           <div className="flex flex-row space-x-4">
-            <label className='flex items-center'>
+            <label className="flex items-center">
               <input
+                {...register("designation", { required: "Select designation" })}
                 type="radio"
                 name="designation"
                 value="hod"
-                checked={designation === 'hod'}
-                onChange={() => setDesignation('hod')}
                 className="form-radio h-6 w-6 text-teal-600 border-teal-400 focus:ring-teal-500"
               />
               <span className="ml-2">HOD</span>
             </label>
-            <label className='flex items-center'>
+            <label className="flex items-center">
               <input
+                {...register("designation", { required: "Select designation" })}
                 type="radio"
                 name="designation"
                 value="invigilator"
-                checked={designation === 'invigilator'}
-                onChange={() => setDesignation('invigilator')}
                 className="form-radio h-6 w-6 text-teal-600 border-teal-400 focus:ring-teal-500"
               />
               <span className="ml-2">Invigilator</span>
@@ -197,7 +206,11 @@ function AddStaff() {
           </Button>
         </form>
       </div>
-      <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+      />
     </div>
   );
 }
