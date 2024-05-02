e148ce73b6813afdc7960c8f0eefab6151bb6cab
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-05-02 12:07:58 +0530

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 import AddStaff from "./staff/AddStaff";
 import ViewStaff from "./staff/ViewStaff";
 import AddDept from "./dept/AddDept";
@@ -21,35 +21,152 @@ import EditProfile from "./EditProfile";
 import ChangePassword from "./ChangePassword";
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />}></Route>
-          <Route path='/temp' element={<Temp />}></Route>
-          <Route path='/dashboard/admin' element={<AdminLayout><AdminDash /></AdminLayout>}></Route>
-          <Route path='/dashboard/hod' element={<HodLayout><HodDash /></HodLayout>}></Route>
-          <Route path='/hod/password' element={<HodLayout><ChangePassword /></HodLayout>}></Route>
-          <Route path='/admin/password' element={<AdminLayout><ChangePassword /></AdminLayout>}></Route>
-          <Route path='/admin/profile' element={<AdminLayout><EditProfile /></AdminLayout>}></Route>
-          <Route path='/hod/profile' element={<HodLayout><EditProfile /></HodLayout>}></Route>
-          <Route path='/staff/add' element={<AdminLayout><AddStaff /></AdminLayout>}></Route>
-          <Route path='/staff/view' element={<AdminLayout><ViewStaff /></AdminLayout>}></Route>
-          <Route path='/dept/add' element={<AdminLayout><AddDept /></AdminLayout>}></Route>
-          <Route path='/dept/view' element={<AdminLayout><ViewDept /></AdminLayout>}></Route>
-          <Route path='/room/add' element={<AdminLayout><AddRoom /></AdminLayout>}></Route>
-          <Route path='/room/view' element={<AdminLayout><ViewRoom /></AdminLayout>}></Route>
-          <Route path='/duty/request' element={<AdminLayout><DutyRequest /></AdminLayout>}></Route>
-          <Route path='/view/hod/approved' element={<HodLayout><ViewApprovedHod /></HodLayout>}></Route>
-          <Route path='/view/admin/approved' element={<AdminLayout><ViewApproved /></AdminLayout>}></Route>
-          <Route path='/duty/approve' element={<HodLayout><DutyApprove /></HodLayout>}></Route>
-          <Route path='/duty/:requestId' element={<HodLayout><StaffApprove /></HodLayout>}></Route>
+          <Route path="/" element={<Login />} />
+          <Route path="/temp" element={<Temp />} />
+          <Route
+            path="/dashboard/admin"
+            element={
+              <AdminLayout>
+                <AdminDash />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/dashboard/hod"
+            element={
+              <HodLayout>
+                <HodDash />
+              </HodLayout>
+            }
+          />
+          <Route
+            path="/hod/password"
+            element={
+              <HodLayout>
+                <ChangePassword />
+              </HodLayout>
+            }
+          />
+          <Route
+            path="/admin/password"
+            element={
+              <AdminLayout>
+                <ChangePassword />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/admin/profile"
+            element={
+              <AdminLayout>
+                <EditProfile />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/hod/profile"
+            element={
+              <HodLayout>
+                <EditProfile />
+              </HodLayout>
+            }
+          />
+          <Route
+            path="/staff/add"
+            element={
+              <AdminLayout>
+                <AddStaff />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/staff/view"
+            element={
+              <AdminLayout>
+                <ViewStaff />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/dept/add"
+            element={
+              <AdminLayout>
+                <AddDept />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/dept/view"
+            element={
+              <AdminLayout>
+                <ViewDept />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/room/add"
+            element={
+              <AdminLayout>
+                <AddRoom />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/room/view"
+            element={
+              <AdminLayout>
+                <ViewRoom />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/duty/request"
+            element={
+              <AdminLayout>
+                <DutyRequest />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/view/hod/approved"
+            element={
+              <HodLayout>
+                <ViewApprovedHod />
+              </HodLayout>
+            }
+          />
+          <Route
+            path="/view/admin/approved"
+            element={
+              <AdminLayout>
+                <ViewApproved />
+              </AdminLayout>
+            }
+          />
+          <Route
+            path="/duty/approve"
+            element={
+              <HodLayout>
+                <DutyApprove />
+              </HodLayout>
+            }
+          />
+          <Route
+            path="/duty/:requestId"
+            element={
+              <HodLayout>
+                <StaffApprove />
+              </HodLayout>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
